1a3f36abca1c5015f680a96ab787576140406140
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-08-09 12:16:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[currentValue,setCurrentValue]=useState(0);
+  const [sum, setSum] = useState(0);
+
+  useEffect(()=>{
+    setSum(prevsum=>prevsum+currentValue)
+
+  },[currentValue])
+
   return (
     <div>
+      <h1>Sum Calculator</h1>
+      <input type="number" value={currentValue} onChange={(e)=>{
+        setCurrentValue(Number(e.target.value))
+
+      }}></input>
+    <br/>
+      Sum:{sum}
+
         {/* Do not remove the main div */}
     </div>
   )
